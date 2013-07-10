f7a0867f4c8eb1b921be838658263e8ac1a1ee63
taptap <alexey.e.egorov@gmail.com>
2013-07-10 07:38:50 +0400

    added database package and file

--- beon.asd
+++ beon.asd
@@ -8,6 +8,7 @@
 (defsystem :beon-tools
   :components ((:file "packages")
 	       (:file "tools")
+	       (:file "database")
 	       (:file "user"))
   :depends-on (:closure-html
 	       :drakma
--- packages.lisp
+++ packages.lisp
@@ -2,9 +2,14 @@
   (:use :cl)
   (:export :make-diary-url
 	   :make-user-info-url
-	   :referer-query))  
+	   :referer-query
+	   :*database*))  
 
 (defpackage :user
   (:use :cl :tools)
   (:export :get-online-users
 	   :fill-user-info))
+
+(defpackage :database
+  (:use :cl :tools :sqlite))
+
