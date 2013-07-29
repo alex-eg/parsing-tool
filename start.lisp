1d9ec418f7026da80f690e3415f3c6fef566c472
taptap <alexey.e.egorov@gmail.com>
2013-07-29 19:26:24 +0400

    added sbcl initialization

--- start.lisp
+++ start.lisp
@@ -1,5 +1,8 @@
 #!/usr/bin/sbcl --script 
 
+(load #P"/etc/sbclrc")
+(load (merge-pathnames (user-homedir-pathname) #P"/.sbclrc"))
+
 (load #P"parser.asd")
 (asdf:operate 'asdf:load-op 'parsing-tools)
-(user:capture)
+(user:capture (cadr sb-ext:*posix-argv*))
