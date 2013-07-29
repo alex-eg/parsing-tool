0b42a61c2d11f8615a155272ad9650c67f300a20
alex_e <alexey.e.egorov@gmail.com>
2013-07-29 10:24:29 +0400

    added replace-all function, provided base-url choosing for other functions

--- tools.lisp
+++ tools.lisp
@@ -1,15 +1,23 @@
 (in-package :tools)
 
-(defparameter *database*  (merge-pathnames *default-pathname-defaults* #P"./database.sqlite"))
+(defparameter *database-path*  (merge-pathnames *default-pathname-defaults* #P"./database.sqlite"))
 
-(defun make-diary-url (username)
-  (concatenate 'string "http://" username ".beon.ru/"))
+(defun make-diary-url (username base-url)
+  (concatenate 'string "http://" username "." base-url "/"))
 
-(defun make-user-info-url (username)
-  (concatenate 'string "http://beon.ru/users/" username "/"))
+(defun make-user-info-url (username base-url)
+  (concatenate 'string "http://" base-url "/users/" username "/"))
 
 (defmacro referer-query (referer useragent usernames)
   (let ((uname (gensym)))
     `(dolist (,uname ,usernames)
        (drakma:http-request (make-diary-url ,uname) :user-agent ,useragent
 			    :additional-headers '(("Referer" . ,referer))))))
+
+(defun replace-all (string char replacement-string)
+  (let ((list (coerce string 'list)))
+    (with-output-to-string (out)
+      (dolist (c list)
+	(if (char-equal c char)
+	    (write-string replacement-string out)
+	    (write-char c out))))))
