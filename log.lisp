ec02e7797dd2fc8f637945271b2b6087c662e8f0
alex_e <alexey.e.egorov@gmail.com>
2013-07-29 10:22:26 +0400

    small bug fixes, added timestamp

--- log.lisp
+++ log.lisp
@@ -1,7 +1,29 @@
 (in-package :log)
 
-(defparameter *logfile* *standard-output*)
-(defparameter *loglevel* :error)
+(defparameter *logfile* '*standard-output*)
+(defparameter *loglevel* :debug)
+
+(defun format-date ()
+  (macrolet ((format-2-digits (name)
+	       `(if (= (mod ,name 10) ,name)
+		    (format nil "0~A" ,name)
+		    (format nil "~A" ,name))))
+    (multiple-value-bind (raw-seconds raw-minutes raw-hours
+				      raw-day raw-month year)
+	(get-decoded-time)
+      (let ((seconds (format-2-digits raw-seconds))
+	    (minutes (format-2-digits raw-minutes))
+	    (hours (format-2-digits raw-hours))
+	    (day (format-2-digits raw-day))
+	    (month (format-2-digits raw-month)))
+	(format nil "~A-~A-~A ~A:~A:~A"
+		day
+		month
+		year
+		hours
+		minutes
+		seconds)))))
+    
 
 (defmacro write-log (level message)
   (let* ((log-levels (list :debug :info :warning :error :critical))
@@ -10,4 +32,8 @@
 	(error "~A is not a valid loglevel" (symbol-name level)))
     (if (>= (position level log-levels)
 	    current-level)
-	`(format ,*logfile* "[~A] ~A" (symbol-name ,level) ,message))))
+	`(format ,*logfile* "[~A] [~A] ~A~%" 
+		 (format-date)
+		 (symbol-name ,level) ,message))))
+
+	
