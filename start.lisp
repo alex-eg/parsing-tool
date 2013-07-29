df11d765ddd2ea4fe9b6527ae242db9cff0a99df
taptap <alexey.e.egorov@gmail.com>
2013-07-29 19:43:42 +0400

    fixed useless output suppression

--- start.lisp
+++ start.lisp
@@ -1,17 +1,17 @@
 #!/usr/bin/sbcl --script 
-(unless (equalp
-         (lisp-implementation-type)
-         "SBCL")
+(unless (equalp (lisp-implementation-type)
+		"SBCL")
   (write-line "Sorry, currently SBCL is required to operate this script")
   (quit))
 
 (load #P"/etc/sbclrc")
-(load (merge-pathnames (user-homedir-pathname) #P"/.sbclrc")
+(load (merge-pathnames (user-homedir-pathname) #P"/.sbclrc"))
 
 (load #P"parser.asd")
-(let ((*standard-output* nil))
+(with-open-file (*standard-output* "/dev/null" :direction :output
+                                   :if-exists :supersede)
   (asdf:operate 'asdf:load-op 'parsing-tools))
 (handler-case
     (user:capture (cadr sb-ext:*posix-argv*))
   (SB-SYS:INTERACTIVE-INTERRUPT ()
-      (write-line "User interrupt, exitting")))
+    (write-line "User interrupt, exitting")))
