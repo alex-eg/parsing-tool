fc6f52e04198ab56386ecb5750030cc8ecb6d8a2
alex_e <alexey.e.egorov@gmail.com>
2013-07-29 10:20:22 +0400

    changed database structure

--- database.lisp
+++ database.lisp
@@ -3,12 +3,12 @@
 (defun initialize-database ()
   "Initialize database, create and/or validate schema"
   (write-log :info "Initializing database...")
-  (sqlite:with-open-database  (db *database*)
+  (sqlite:with-open-database  (db *database-path*)
     (sqlite:execute-single 
      db
      "CREATE TABLE IF NOT EXISTS user(
-id INTEGER UNIQUE NOT NULL PRIMARY KEY,
-name VARCHAR(255),
+name VARCHAR(512) UNIQUE NOT NULL PRIMARY KEY,
+display_name VARCHAR(512), 
 date_created DATETIME,
 email VARCHAR(255),
 date_of_birth VARCHAR(255),
@@ -19,15 +19,14 @@ last_active DATETIME);")
     (sqlite:execute-single
      db
      "CREATE TABLE IF NOT EXISTS interest(
-id INTEGER UNIQUE NOT NULL PRIMARY KEY,
-name VARCHAR(255),
-real_id INTEGER);") ;; Real id - for things like l33t and leet and different writings of same word
+name VARCHAR(1024) UNIQUE NOT NULL PRIMARY KEY,
+real_name VARCHAR(1024));") ;; Real id - for things like l33t and leet and different spellings of same word
     (sqlite:execute-single
      db
      "CREATE TABLE IF NOT EXISTS interests_users_map(
-user_id INTEGER,
-interest_id INTEGER,
-FOREIGN KEY (user_id) REFERENCES user(id) ON UPDATE CASCADE ON DELETE CASCADE,
-FOREIGN KEY (interest_id) REFERENCES interest(id));")))
-
-
+user_name VARCHAR(512),
+interest_name VARCHAR(1024),
+FOREIGN KEY (user_name) REFERENCES user(name) ON UPDATE CASCADE ON DELETE CASCADE,
+FOREIGN KEY (interest_name) REFERENCES interest(name));")))
+  
+		      
