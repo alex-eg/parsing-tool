471e9fab930f6e2bd014c1f3fe03cff0bd03484b
taptap <alexey.e.egorov@gmail.com>
2015-07-23 11:27:54 +0300

    add command-line-arguments to :use section of main package

--- packages.lisp
+++ packages.lisp
@@ -1,6 +1,7 @@
 (defpackage :parsing-tool
   (:use :cl :postmodern
         :simple-date
-        :split-sequence)
+        :split-sequence
+        :command-line-arguments)
   (:export :capture
            :get-and-store-online))
